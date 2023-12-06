48295aa326e190c610148a8daf81e7d2a7d12fb7
Sinouhé Soupart <sinouhes@gmail.com>
2023-12-06 18:59:26 +0100

    feat: day 6 part 2

--- Day 6/day6-2.ts
+++ Day 6/day6-2.ts
@@ -1,7 +1,27 @@
 import * as fs from "fs";
 
 function main() {
-    const data: string = fs.readFileSync('data.txt').toString();
+    const data = fs.readFileSync('data.txt').toString();
+    const time: number = parseInt([...[...data.matchAll(/Time:[0-9\s]+/g)][0][0].matchAll(/[0-9]+/g)].map((m) => m[0]).reduce((a: string, b: string) => a + b));
+    const distance: number = parseInt([...[...data.matchAll(/Distance:[0-9\s]+/g)][0][0].matchAll(/[0-9]+/g)].map((m) => m[0]).reduce((a: string, b: string) => a + b));
+
+    console.log('total:', getValue(time, distance))
 }
+
+function getValue(time: number, distance: number): number {
+    const sols: number[] = getQuadraticSolutions(time, distance);
+    if(!sols.length) return 0;
+    if(sols.length == 1) return 1;
+    else return (Math.ceil(sols[1] - 1) - Math.floor(sols[0] + 1) + 1);
+}
+
+function getQuadraticSolutions(time: number, distance: number): number[] {
+    // equation is t0² - t*t0 + d = 0
+    // in general, solutions are (-b ± √b²-4ac)/2, in this case (t ± √t²-4d)/2
+    const delta = (time * time ) - (4 * distance);
+    if(delta < 0) return [];
+    if(delta == 0) return [time/2];
+    return [(time - Math.sqrt(delta))/2, (time + Math.sqrt(delta))/2];
+}   
 
 main();
